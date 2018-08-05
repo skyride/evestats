deadf678ba2473abd4596823f905352c4e8d9610
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-08-05 19:55:59 +0100

    Updated debug settings

--- evestats/settings.py
+++ evestats/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v+h%5!kvkeyq6ii=pkneme#11%e-&udmnf2b#x_+@uq1#b!199'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 
